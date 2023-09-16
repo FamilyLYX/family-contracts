c01f41484ae05f5a62ec2323c986c72b47b09ba3
Shashank <kriss1897@users.noreply.github.com>
2023-09-16 22:25:50 +0530

    Add some dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import "solidity-docgen";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  docgen: {
+    pages: 'files',
+    pageExtension: '.md'
+  }
 };
 
 export default config;
