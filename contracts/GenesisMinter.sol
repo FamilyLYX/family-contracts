cf1730ae4effe4a5bea57cf20a9e90a2d29f935b
Mumeen <azeezmumeen6@.com>
2024-01-09 16:48:44 +0100

    add genesis collection minting to the minter

--- contracts/GenesisMinter.sol
+++ contracts/GenesisMinter.sol
@@ -10,6 +10,15 @@ interface IGenesisPerk {
     function mint(address receiver) external;
 }
 
+interface IGenesisCollection {
+    function mint(
+        address to,
+        bytes12 variantId,
+        bool allowNonLSP1Recipient,
+        bytes memory data
+    ) external;
+}
+
 contract GenesisMinter {
     address internal genesisCollection;
     address internal genesisPerk;
@@ -28,60 +37,23 @@ contract GenesisMinter {
         genesisPerk = perk;
     }
 
-    function mintGenesis(string memory uid, bytes memory signature) external {
+    function mintGenesis(
+        address to,
+        bytes12 variantId,
+        bool allowNonLSP1Recipient,
+        bytes memory data,
+        string memory uid
+    ) external {
         require(mintedUsers[uid] == false, "User already minted");
-        bytes32 messageHash = keccak256(
-            bytes.concat(_MSG_HASH_PREFIX, bytes(uid))
+        require(msg.sender == owner, "Only FamilyUP can mint");
+
+        IGenesisCollection(genesisCollection).mint(
+            to,
+            variantId,
+            allowNonLSP1Recipient,
+            data
         );
-        if (_isValidSignature(messageHash, signature) != _ERC1271_MAGICVALUE) {
-            revert InvalidFamilySignature();
-        }
-
-        //TODO: add genesis mint function call
-
-        IGenesisPerk(genesisPerk).mint(msg.sender);
+        IGenesisPerk(genesisPerk).mint(to);
         mintedUsers[uid] = true;
-    }
-
-    function _isValidSignature(
-        bytes32 dataHash,
-        bytes memory signature
-    ) internal view returns (bytes4 magicValue) {
-        address _owner = owner;
-
-        // If owner is a contract
-        if (_owner.code.length > 0) {
-            (bool success, bytes memory result) = _owner.staticcall(
-                abi.encodeWithSelector(
-                    IERC1271.isValidSignature.selector,
-                    dataHash,
-                    signature
-                )
-            );
-
-            bool isValid = (success &&
-                result.length == 32 &&
-                abi.decode(result, (bytes32)) == bytes32(_ERC1271_MAGICVALUE));
-
-            return isValid ? _ERC1271_MAGICVALUE : _ERC1271_FAILVALUE;
-        }
-        // If owner is an EOA
-        else {
-            // if isValidSignature fail, the error is catched in returnedError
-            (address recoveredAddress, ECDSA.RecoverError returnedError) = ECDSA
-                .tryRecover(dataHash, signature);
-
-            // if recovering throws an error, return the fail value
-            if (returnedError != ECDSA.RecoverError.NoError)
-                return _ERC1271_FAILVALUE;
-
-            // if recovering is successful and the recovered address matches the owner's address,
-            // return the ERC1271 magic value. Otherwise, return the ERC1271 fail value
-            // matches the address of the owner, otherwise return fail value
-            return
-                recoveredAddress == _owner
-                    ? _ERC1271_MAGICVALUE
-                    : _ERC1271_FAILVALUE;
-        }
     }
 }
